a5844a8cabacc0e8df852e6ca11cb9f471cd21e3
CaioBrandz <caiobrandz@gmail.com>
2022-07-07 08:18:16 -0300

    feat: ingredients list, buttons and average

--- DeGusteauApp/src/screens/RecipeScreen.js
+++ DeGusteauApp/src/screens/RecipeScreen.js
@@ -9,34 +9,85 @@ export function RecipeScreen({navigation,route}) {
 
   const [isLoadingReceita, setLoadingReceita] = useState(true);
   const [receitaData, setReceitaData] = useState([]);
-  console.log(receitaData);
+ 
+  const [isLoadingReceitaIngredientes, setLoadingReceitaIngredientes] = useState(true);
+  const [receitaIngredientesData, setReceitaIngredientesData] = useState([]);
 
+  const [isLoadingMedia, setLoadingMedia] = useState(true);
+  const [mediaData, setMediaData] = useState([]);
+  console.log(mediaData);
+  
   const getReceita = async () => {
     try {
-     const response = await fetch(`http://192.168.18.5:5000/receitas/${route.params.id_receita}`, {
-       method: 'GET',
-       headers: {
-         Accept: 'application/json',
-         'Content-Type': 'application/json',
-       }}
-     );
-     const json = await response.json();
-     setReceitaData(json);
-   } catch (error) {
-     console.error(error);
-   } finally {
-    setLoadingReceita(false);
-   }
- };
+      const response = await fetch(`http://18.230.138.105:5000/receitas/${route.params.id_receita}`, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        }}
+      );
+      const json = await response.json();
+      setReceitaData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoadingReceita(false);
+    }
+  };
+
+  const getReceitaIngredientes = async () => {
+    try {
+      const response = await fetch(`http://18.230.138.105:5000/receitas/${route.params.id_receita}/ingredientes`, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        }}
+      );
+      const json = await response.json();
+      setReceitaIngredientesData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoadingReceitaIngredientes(false);
+    }
+  };
+
+  const getMedias = async () => {
+    try {
+      const response = await fetch(`http://18.230.138.105:5000/receitas/${route.params.id_receita}/ingredientes/medias`, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        }}
+      );
+      const json = await response.json();
+      setMediaData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoadingMedia(false);
+    }
+  };
+
 
   useEffect(() => {
     getReceita();
   }, []);
 
+  useEffect(() => {
+    getReceitaIngredientes();
+  }, []);
+
+  useEffect(() => {
+    getMedias();
+  }, []);
+
   return (
     <View style={{flex: 1, backgroundColor: '#fff'}}>
-      <Text>Screen C</Text>
-      <Button title="Go to Screen A" onPress={handleNavigateToHomeScreen} />
+      <Text>Estou na Tela da receita selecionada</Text>
+      <Button title="Go to Home" onPress={handleNavigateToHomeScreen} />
 
       <View style={{ flex: 1, padding: 24 }}>
         <Text>Receita:</Text>
@@ -49,6 +100,45 @@ export function RecipeScreen({navigation,route}) {
               <Text>{item.nome}</Text>
               <Text>{item.tempo_preparo}</Text>
               <Text>{item.modo_preparo}</Text>
+              </>
+            )}
+          />
+        )}
+      </View>
+
+      <View style={{ flex: 1, padding: 24 }}>
+        <Text>Ingredientes:</Text>
+        {isLoadingReceitaIngredientes ? <ActivityIndicator/> : (
+          <FlatList
+            data={receitaIngredientesData}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <>
+              <Text>• {item.nome} : {item.quantidade} {item.unidade}</Text>
+              <Button
+                onPress={() =>
+                  navigation.navigate('IngredientPrice', { 
+                    id_ingrediente: item.id,
+                    nome_ingrediente: item.nome,
+                  })
+                }
+                title="ir para o ingrediente"
+              />
+              </>
+            )}
+          />
+        )}
+      </View>
+      
+      <View style={{ flex: 1, padding: 24 }}>
+        <Text>Medias de valores:</Text>
+        {isLoadingMedia ? <ActivityIndicator/> : (
+          <FlatList
+            data={mediaData}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <>
+              <Text>{item.nome} - R${item.media}</Text>
               </>
             )}
           />
