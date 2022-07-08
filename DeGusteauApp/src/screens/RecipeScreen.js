40ff85c82a6fcf7a37f5d4b4e7e05e84fab9d06c
CaioBrandz <caiobrandz@gmail.com>
2022-07-08 08:23:19 -0300

    refactor: recipe screen

--- DeGusteauApp/src/screens/RecipeScreen.js
+++ DeGusteauApp/src/screens/RecipeScreen.js
@@ -1,11 +1,9 @@
 /* eslint-disable prettier/prettier */
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, Button, ActivityIndicator, FlatList } from 'react-native';
+import { View, Text, StyleSheet, Button, ActivityIndicator, FlatList, Image, TouchableHighlight } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
 
-export function RecipeScreen({navigation,route}) {
-  function handleNavigateToHomeScreen() {
-    navigation.navigate('HomeScreen');
-  }
+export function RecipeScreen({navigation,route, navigation: { goBack }}) {
 
   const [isLoadingReceita, setLoadingReceita] = useState(true);
   const [receitaData, setReceitaData] = useState([]);
@@ -15,8 +13,6 @@ export function RecipeScreen({navigation,route}) {
 
   const [isLoadingMedia, setLoadingMedia] = useState(true);
   const [mediaData, setMediaData] = useState([]);
-  console.log(mediaData);
-  
   const getReceita = async () => {
     try {
       const response = await fetch(`http://18.230.138.105:5000/receitas/${route.params.id_receita}`, {
@@ -86,64 +82,150 @@ export function RecipeScreen({navigation,route}) {
 
   return (
     <View style={{flex: 1, backgroundColor: '#fff'}}>
-      <Text>Estou na Tela da receita selecionada</Text>
-      <Button title="Go to Home" onPress={handleNavigateToHomeScreen} />
-
-      <View style={{ flex: 1, padding: 24 }}>
-        <Text>Receita:</Text>
-        {isLoadingReceita ? <ActivityIndicator/> : (
-          <FlatList
-            data={receitaData}
-            keyExtractor={({ id }, index) => id}
-            renderItem={({ item }) => (
-              <>
-              <Text>{item.nome}</Text>
-              <Text>{item.tempo_preparo}</Text>
-              <Text>{item.modo_preparo}</Text>
-              </>
-            )}
-          />
-        )}
-      </View>
-
-      <View style={{ flex: 1, padding: 24 }}>
-        <Text>Ingredientes:</Text>
-        {isLoadingReceitaIngredientes ? <ActivityIndicator/> : (
-          <FlatList
-            data={receitaIngredientesData}
-            keyExtractor={({ id }, index) => id}
-            renderItem={({ item }) => (
-              <>
-              <Text style={{color: '#444'}}>• {item.nome} : {item.quantidade} {item.unidade}</Text>
-              <Button
-                onPress={() =>
-                  navigation.navigate('IngredientPrice', { 
-                    id_ingrediente: item.id,
-                    nome_ingrediente: item.nome,
-                  })
-                }
-                title="ir para o ingrediente"
-              />
-              </>
-            )}
-          />
-        )}
-      </View>
       
-      <View style={{ flex: 1, padding: 24 }}>
-        <Text>Medias de valores:</Text>
-        {isLoadingMedia ? <ActivityIndicator/> : (
-          <FlatList
-            data={mediaData}
-            keyExtractor={({ id }, index) => id}
-            renderItem={({ item }) => (
-              <>
-              <Text>{item.nome} - R${item.media}</Text>
-              </>
-            )}
-          />
-        )}
-      </View>
+      {isLoadingReceitaIngredientes ? (
+        <ActivityIndicator /> 
+        ) : ( 
+                
+        <FlatList style={{color: '#444'}} 
+          ListHeaderComponent={
+            <View style={{ flex: 1, paddingHorizontal: 24, paddingVertical: 14 }}>
+              {isLoadingReceita ? <ActivityIndicator/> : (
+
+                <FlatList 
+                  // DADOS DA RECEITA
+                  data={receitaData }
+                  keyExtractor={({ id }, index) => id}
+                  renderItem={({ item }) => (
+                    <>
+                    <View style={{ flex: 1, width: '100%', flexDirection: 'row',alignItems: 'center', paddingBottom: 8}}>
+                      <TouchableHighlight style={{ borderRadius: 50, padding: 8}} underlayColor="#FFEECA" activeOpacity={0.8} onPress={() => goBack()}>  
+                          <Icon name='leftcircleo' color={'#F54749'} size={24} />
+                      </TouchableHighlight>
+
+                      <Text style={styles.title}> {item.nome}</Text>
+                    </View>
+
+                    <Text><Icon name='clockcircleo' /> {item.tempo_preparo}</Text>
+
+                    <View style={{width: '100%', paddingVertical: 24}}>
+                      <Image style={{width: '100%'}} source={require('../images/video.png')}/>
+                    </View>
+
+                    <View style={{flexDirection: 'row',alignItems: 'center'}}>
+                      <View style={styles.iconFrame}>
+                        <Image style={styles.icon} source={require('../images/icons/chef-hat.png')}/>
+                      </View>
+                      <Text style={styles.subtitle}> Ingredientes:</Text>
+                    </View>
+                    </>
+                  )}
+                />
+              )}
+            </View>
+
+            
+          }
+          
+          data={receitaIngredientesData}
+          keyExtractor={({id}, index) => id}
+          renderItem={({item}) => 
+          // INGREDIENTES - QUANTIDADES
+            <>
+              <View style={{ flex: 1, width: '100%', flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 24}}>
+                <Text style={{color: '#444', width: '55%' }}>• {item.nome[0].toUpperCase() + item.nome.substring(1)} : {item.quantidade} {item.unidade}</Text>
+                
+                <TouchableHighlight style={{ borderRadius: 16, padding: 8}} underlayColor="#FFEECA"  activeOpacity={0.8} onPress={() =>
+                    navigation.navigate('IngredientPrice', { 
+                      id_ingrediente: item.id,
+                      nome_ingrediente: item.nome,
+                    })}>
+                    <View>
+                      <Text style={{color: '#F54749'}}><Icon name='pushpino' color={'#F54749'} size={14} /> Onde encontrar </Text>
+                    </View>
+                </TouchableHighlight>
+              </View>
+            </>
+          }
+          
+          ListFooterComponent={
+            <View style={{flex: 1, paddingHorizontal: 24, paddingVertical:14 }}> 
+                    
+              <Text style={styles.subtitle}><Icon name='linechart' color={'#F54749'} size={18} /> Médias de valores:</Text>
+              
+              {isLoadingMedia ? <ActivityIndicator/> : (
+
+                <FlatList 
+                  // INGREDIENTES - VALORES
+                  data={mediaData}
+                  style={{ flex: 1, width:'100%',flexDirection: 'row'}}
+                  keyExtractor={({ nome }, index) => nome}
+                  listKey={({ nome }, index) => nome}
+                  renderItem={({ item }) => (
+                    <>
+                      <View style={{ alignItems: 'center', paddingRight: 24, paddingTop: 14}}>
+                          <Text style={styles.corpo}>{item.nome[0].toUpperCase() + item.nome.substring(1)}</Text>
+                          <Text style={styles.corpo}>R${item.media}</Text>
+                      </View>
+                    </>
+                  )}
+                />
+              )}
+              <View style={{flex: 1, paddingVertical: 28 }}>
+                <View style={{flexDirection: 'row',alignItems: 'center'}}>
+                  <View style={styles.iconFrame}>
+                    <Image style={styles.icon} source={require('../images/icons/preparo.png')}/>
+                  </View>
+                  <Text style={styles.subtitle}> Modo de preparo:</Text>
+                </View>
+                {isLoadingReceita ? <ActivityIndicator/> : (
+
+                  <FlatList 
+                    // MODO DE PREPARO
+                    data={receitaData }
+                    keyExtractor={({ id }, index) => id}
+                    listKey={({ id }, index) => id}
+                    renderItem={({ item }) => (
+                      <>
+                      <View style={{flex: 1, paddingVertical: 14 }}>
+                        <Text style={styles.corpo}>{item.modo_preparo}</Text>
+                      </View>
+                      </>
+                    )}
+                  />
+                )}
+              </View>
+            </View>
+      
+          }
+        />
+      )} 
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  headerWrapper:{
+      flex: '1'
+  },
+  title:{
+      fontSize: 32,
+      color: '#444'
+  },
+  subtitle:{
+    fontSize: 22,
+    color: '#444'
+  },
+  corpo:{
+    fontSize: 14,
+    color: '#444'
+  },
+  iconFrame: {
+    width:'5%',
+    aspectRatio: 1
+  },
+  icon: {
+    width:'100%',
+    height:'100%'
+  },
+})
